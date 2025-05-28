d3cd5c10f66610980b529dd2831019c323f26e1b
Xateam <noori030924@naver.com>
2025-05-28 23:28:31 +0900

    ✨ feat: DB에 UTC로 저장된 시간을 조회 시 KST로 변환 처리
    - expires_at 필드를 UTC 기준으로 DB에 저장
    - 사용자에게 보여줄 때는 KST(Asia/Seoul) 기준으로 변환하여 출력
    - 시간대 혼동 방지를 위한 명시적 타임존 처리

--- vibeapp/__init__.py
+++ vibeapp/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
+
 
 from vibeapp.extensions import db
 from vibeapp.config import Config
@@ -21,5 +24,15 @@ def create_app():
     with app.app_context():
         db.create_all() # DB 테이블 생성
         
+        
+    def kst_format(dt):
+        if dt is None:
+            return "없음"
+        if dt.tzinfo is None:
+            dt = dt.replace(tzinfo=timezone.utc)
+        kst_time = dt.astimezone(ZoneInfo("Asia/Seoul"))
+        return kst_time.strftime("%Y-%m-%d %H:%M:%S")
+    
+    app.jinja_env.filters['kst'] = kst_format
 
     return app
