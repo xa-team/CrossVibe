bc180caf7a35e4fe3810ab455eb0fb4c8313eb0f
Xateam <noori030924@naver.com>
2025-05-29 21:37:07 +0900

    ♻️ refactor: token.token_expire_at → token.expire_at으로 변수명 변경
    - 중복된 표현(token.token_expire_at)의 가독성 개선을 위해 expire_at으로 변수명 변경
    - PlatformToken 모델의 필드 이름을 더 명확하고 간결하게 수정
    - 관련 참조 코드 일괄 수정

--- vibeapp/models/platform_token.py
+++ vibeapp/models/platform_token.py
@@ -6,7 +6,7 @@ class PlatformToken(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     access_token = db.Column(db.String(255), nullable=False)
     refresh_token = db.Column(db.String(255), nullable=True)
-    token_expire_at = db.Column(db.DateTime, nullable=True)
+    expire_at = db.Column(db.DateTime, nullable=True)
     extra_data = db.Column(db.JSON, nullable=True)
 
     platform_connection = db.relationship("PlatformConnection", back_populates="token", uselist=False)
