d7535b3cfc4bc4ba31b9bdb97fa800b716c423b7
Xateam <noori030924@naver.com>
2025-06-04 11:26:49 +0900

    ♻️ refactor: 플랫폼별 PlaylistService 구조를 추상 클래스 기반으로 통일
    - BasePlaylistService 추상 클래스를 도입하여 플랫폼별 PlaylistService 인터페이스 통일
    - SpotifyPlaylistService는 BasePlaylistService를 상속해 실제 구현
    - factory.get_playlist_service(connection) 함수로 플랫폼별 PlaylistService를 생성 및 반환
    - 중복 방지를 위해 spotify_id + platform_connection_id 기준으로 식별
    - 멀티 플랫폼 확장성을 고려해 구조적 분기 처리 정립
    - SRP 원칙을 준수하도록 토큰 로직과 플레이리스트 로직을 명확히 분리

--- vibeapp/services/factory.py
+++ vibeapp/services/factory.py
@@ -1,4 +1,6 @@
+from vibeapp.models.platform_connection import PlatformConnection
 from vibeapp.services.spotify.auth_service import SpotifyAuthService
+from vibeapp.services.spotify.playlist_service import SpotifyPlaylistService
 #추후 다른 플랫폼 추가 가능
 
 def get_auth_service(platform: str):
@@ -8,3 +10,13 @@ def get_auth_service(platform: str):
     #   return YoutubeAuthService
     else:
         raise NotImplementedError(f"{platform} AuthService is not implemented.")
+    
+def get_playlist_service(connection: PlatformConnection):
+    platform = connection.platform.lower()
+    
+    if platform == "spotify":
+        return SpotifyPlaylistService()
+    #elif platform == "youtube";
+    #   return YoutubePlaylistService()
+    else:
+        raise NotImplementedError(f"{platform} PlaylistService not implemented.")
